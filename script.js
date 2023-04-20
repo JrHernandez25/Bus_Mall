d8c9b4e0631f634df29e1da9e6c6df8deefdf486
Jr Hernandez <jrhernande25@g.crookcountyschools.org>
2023-04-20 13:58:09 -0700

    Tried to add function

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict'
-console.log('Hello world');
+
+const img1 = document.getElementById("img1")
+
+img1.addEventListener('click', function () {
+const newimg = document.createElement('img')
+newimg.src = '/Images/Img/bubblegum.jpg;'
+const image = document.getElementById('div');
+image.appendChild(newimg);
+});
