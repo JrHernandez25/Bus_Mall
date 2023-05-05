b025ba3623bccc233a81aded50504c5b04f5b788
Jr Hernandez <jrhernande25@g.crookcountyschools.org>
2023-05-05 13:53:11 -0700

    Added to the functionallity

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-'use strict'
+'use strict';
 
 const ImageContainer = document.getElementById('s3');
-const img1 = document.getElementById('img1');
-const img2 = document.getElementById('img2');
-const img3 = document.getElementById('img3');
+const image1 = document.getElementById('img1');
+const image2 = document.getElementById('img2');
+const image3 = document.getElementById('img3');
 const Tone = document.getElementById('T1');
 const Ttwo = document.getElementById('T2');
 const Tthree = document.getElementById('T3');
-
+let count = 0;
 function Image (name, image) {
     this.name = name;
     this.image = image;
@@ -31,7 +31,7 @@ Image.all.push (new Image('pen', './Img/pen.jpg'));
 Image.all.push (new Image('pet-sweep', './Img/pet-sweep.jpg'));
 Image.all.push (new Image('scissors', './Img/scissors.jpg'));
 Image.all.push (new Image('shark', './Img/shark.jpg'));
-Image.all.push (new Image('sweep', './Img/sweep.jpg'));
+Image.all.push (new Image('sweep', './Img/sweep.png'));
 Image.all.push (new Image('tauntaun', './Img/tauntaun.jpg'));
 Image.all.push (new Image('unicorn', './Img/unicorn.jpg'));
 Image.all.push (new Image('water-can', './Img/water-can.jpg'));
@@ -40,4 +40,80 @@ Image.all.push (new Image('wine-glass', './Img/wine-glass.jpg'));
 
 
 
-console.log(Image.all);
+
+Image.prototype.renderImages = function (image, name) {
+ image.src = this.image;
+ name.textContent = this.name;
+ this.views ++;
+};
+
+function randomizeArray(){
+    let myArray = Image.all;
+    let randomArray = myArray.sort(function(){return (Math.random()-0.5)});
+    console.log(randomArray)
+};
+
+function getThreeImages(){
+    randomizeArray();
+    let img1 = Image.all[0];
+    let img2 = Image.all[1];
+    let img3 = Image.all[2];
+    img1.renderImages(image1, Tone);
+    img2.renderImages(image2, Ttwo);
+    img3.renderImages(image3, Tthree);
+};
+
+
+function putArrayInStorage(){
+    let stringArray = JSON.stringify(Image.all);
+    if (stringArray = []){
+        localStorage.setItem('Image', stringArray);
+    }
+    else(localStorage.setItem('Image', stringArray));
+}
+
+
+function getArrayFromStorage(){
+    let storedImage = localStorage.getItem('Image');
+    if(storedImage){
+        let newImage = JSON.parse(storedImage);
+        for(let images of newImage){
+            let myNewImage = new Images(images.name, images.imgPath, images.views, images.clicks);
+            Image.all.push(myNewImage)
+        }
+    }
+}
+function removeImages (){
+    document.getElementById("img1").style.display='none';
+    document.getElementById("img2").style.display='none';
+    document.getElementById("img3").style.display='none';
+}
+function handleClick(e){
+    let imageClicked = e.target.id;
+    if(imageClicked === 'img1' || imageClicked === 'img2' || imageClicked === 'img3'){
+        count++;
+    }
+    if(imageClicked === 'img1'){
+        Image.all[0].clicks++;
+    }    
+    if(imageClicked === 'img2'){
+        Image.all[1].clicks++;
+    }
+    if(imageClicked === 'img3'){
+        Image.all[2].clicks++;
+    } 
+    getThreeImages(); 
+    if (count === 25){
+        removeImages();
+        document.write('<h1>The test is over now</h1>');
+        console.log(Image.all);
+    }
+    putArrayInStorage();
+};
+
+
+
+ImageContainer.addEventListener('click', handleClick)
+console.log(Image);
+getArrayFromStorage();
+getThreeImages();
